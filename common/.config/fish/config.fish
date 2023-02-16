5087980c1783ff84084c1f7ae5679c52c5e38890
Seth Holladay <me@seth-holladay.com>
2023-02-16 13:20:33 -0500

    Add config for fish shell

---
+++ common/.config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    ~/Code/sh/greet &;
+    # Make "ls" show hidden files.
+    alias ls='ls -AG';
+    # Make "dir" show permissions and friendly file sizes.
+    alias dir='ls -hl';
+end
