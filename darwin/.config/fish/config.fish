2bcd3a5941dbf72dfbdbf223c635b71e8265bf04
Seth Holladay <me@seth-holladay.com>
2025-08-19 10:38:14 -0400

    Use nano as the default text editor

--- darwin/.config/fish/config.fish
+++ darwin/.config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     "$HOME/Code/sh/greet" &;
     echo -e '\nWelcome to the Matrix.\n';
+    set -gx EDITOR 'nano';
     # Make "ls" show hidden files.
     alias ls='ls -AG';
     # Make "dir" show permissions and friendly file sizes.
--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     echo -e '\nWelcome to the Matrix.\n';
+    set -gx EDITOR 'nano';
     # Make "ls" show hidden files.
     alias ls='ls -AG';
     # Make "dir" show permissions and friendly file sizes.
